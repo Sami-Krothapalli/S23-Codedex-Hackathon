b9aa81c71491296fff926d39e4dc5273dd44c069
KatarinaStjepanovic <165832565+KatarinaStjepanovic@users.noreply.github.com>
2024-07-12 06:14:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const buttons = document.querySelectorAll('.menu button');
+
+    buttons.forEach( (button) => {
+        button.addEventListener('click', () => {
+            const targetId = button.getAttribute('data-target');
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView( {behavior : 'smooth'});
+        })
+    })
+})
+
+
 const unnecessaryClass = document.getElementsByClassName('card');
 const invitation = unnecessaryClass[0];
-
+  
   function displayInvitation (){
    const envelope =  document.getElementsByClassName('container')[0];
   envelope.remove();
-  const newInvite = document.createElement('div');
-  newInvite.textContent = 'Hello!';
-  newInvite.classList = 'letter';
+  const letter = document.getElementsByClassName('letter')[0];
+  letter.style.display = 'block';
+  document.body.style.backgroundColor = '#9cf6fb';
+  
 
    document.body.appendChild(newInvite);
     console.log("It works!");
 }
+
