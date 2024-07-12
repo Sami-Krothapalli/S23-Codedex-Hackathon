5e3e04d697425c7b6420ab993dd6910096330c1b
KatarinaStjepanovic <165832565+KatarinaStjepanovic@users.noreply.github.com>
2024-07-12 15:15:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,9 +20,60 @@ const invitation = unnecessaryClass[0];
   const letter = document.getElementsByClassName('letter')[0];
   letter.style.display = 'block';
   document.body.style.backgroundColor = '#9cf6fb';
-  
+ 
+}
 
-   document.body.appendChild(newInvite);
-    console.log("It works!");
+function goToMap() {
+    const map = document.getElementById('map');
+    map.scrollIntoView({ behavior: 'smooth' });
+}
+const cardsContainer = document.querySelector(".container");
+
+
+
+
+document.getElementById('shareButton').addEventListener('click', function() {
+    if (navigator.share) {
+        navigator.share({
+            title: 'Scoop Sale Invitation',
+            text: 'Description or text to share',
+            url: 'http://127.0.0.1:5500/', // URL of the shared content
+        })
+        .then(() => console.log('Shared successfully'))
+        .catch((error) => console.error('Sharing failed', error));
+    } else {
+        console.log('Web Share API not supported.');
+       
+    }
+});
+
+function downloadQR() {
+    var qrImage = new Image();
+    qrImage.src = './assets/qrcode.png';
+    var downloadLink = document.createElement('a');
+    downloadLink.href = qrImage.src; 
+    downloadLink.download = 'qr-code.png'; 
+    downloadLink.style.display = 'none'; 
+
+   
+    document.body.appendChild(downloadLink);
+
+
+    downloadLink.click();
+
+  
+    document.body.removeChild(downloadLink);
+}
+
+
+function showCalendar (){
+    const eventDate = '20240820'; 
+    const startTime = '100000'; 
+    const endTime = '150000'; 
+    const calendarUrl = 'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=' +
+                      eventDate + 'T' + startTime + 'Z/' + eventDate + 'T' + endTime + 'Z' +
+                      '&text=Brooklyn%20Stoop%20Sale&location=Court%20Street%20and%202nd%20Ave';
+
+    window.open(calendarUrl, '_blank');
 }
 
